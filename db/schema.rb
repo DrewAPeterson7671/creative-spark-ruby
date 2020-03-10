7b783057f159da44172af28cebe37084ba3c8675
Student <example@example.com>
2020-03-10 16:43:27 -0700

    Fixed the delete crash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_184518) do
+ActiveRecord::Schema.define(version: 2020_03_10_234040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_184518) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
   end
 
 end
