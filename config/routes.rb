9679056a73f72a4c0a0a5223ee20e889be51f3cc
Student <example@example.com>
2020-03-11 17:23:05 -0700

    Adding bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
   root to: 'articles#index'
   resources :articles do
     resources :comments
